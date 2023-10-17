b24e9a8ff99a6ae29fffc7b270b7471d42172756
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-10-17 12:37:22 +0300

    Task 4 was added

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', handleClick);
+
+function handleClick() {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  span.textContent = newColor;
+}
