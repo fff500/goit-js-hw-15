9e2a5a9786f6cc7675dd57b5619de22487cee555
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-10-17 11:56:53 +0300

    Task 3 was initialised

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+function handleForm(event) {
+  console.log(event);
+  event.preventDefault();
+
+  const { name, password } = event.currentTarget.elements;
+
+  const elements = {
+    name: name.value,
+    password: password.value
+  };
+
+  const data = {};
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach(({key, value}) => {
+    data[key] = value;
+  });
+
+  console.log(data);
+}
+
+form.addEventListener('submit', handleForm);
